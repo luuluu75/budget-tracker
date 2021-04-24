05bde68ddf68f1cd7a2fffc05b55fa6b317ad65a
Thu Van <luuluu_1975@yahoo.com.au>
2021-04-24 11:50:22 +1000

    update to offline code

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,11 +2,12 @@ const FILES_TO_CACHE = [
   "/",
   "/index.html",
   "/index.js",
-  "styles.css",
+  "/styles.css",
   "/manifest.webmanifest",
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png",
-  "routes/api.js",
+  "./routes/api.js",
+  "/db.js"
 ];
 
 const CACHE_NAME = "static-cache-v2";
