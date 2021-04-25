bba466c514ee0e2fd5dee518f64e5542354d6fb2
Thu Van <luuluu_1975@yahoo.com.au>
2021-04-25 12:29:20 +1000

    fix for file not found

--- public/db.js
+++ public/db.js
@@ -44,7 +44,7 @@ function checkDatabase() {
 
   getAll.onsuccess = function() {
     if (getAll.result.length > 0) {
-      fetch("/api/transaction/bulk", {
+      fetch("api/transaction/bulk", {
         method: "POST",
         body: JSON.stringify(getAll.result),
         headers: {
