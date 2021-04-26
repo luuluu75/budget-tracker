7b2bcbae6ea403e27c7ebec039c7da57eb8ef0a8
Thu Van <luuluu_1975@yahoo.com.au>
2021-04-26 12:04:10 +1000

    updated code

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,7 +4,7 @@ const FILES_TO_CACHE = [
   "/index.js",
   "/styles.css",
   "/db.js",
-  // "../routes/api.js",
+  "/routes/api.js",
   "/manifest.webmanifest",
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png",
